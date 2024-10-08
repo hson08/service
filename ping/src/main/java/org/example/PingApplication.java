d16b9601a81cb4e6f79672f1f0b83a01dd18c035
陈汉顺 <hson08_chen@163.com>
2024-10-08 11:58:46 +0800

    second

--- ping/src/main/java/org/example/PingApplication.java
+++ ping/src/main/java/org/example/PingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,14 +23,17 @@ public class PingApplication {
         return new RestTemplate();
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(PingApplication.class, args);
 
-        /*WebClient client = WebClient.create("http://localhost:8080");
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(PingApplication.class, args);
+        String port = context.getEnvironment().getProperty("server.port");
+        String url = "http://localhost:"+ port;
+
+        WebClient client = WebClient.create(url);
         Flux.interval(Duration.ofSeconds(2))
                 .flatMap(i -> client.get().uri("/ping?say=Hello").exchange())
                 .flatMap(clientResponse -> clientResponse.bodyToMono(String.class))
-                .subscribe(response -> System.out.println("Response from Pong: " + response));*/
+                .subscribe(response -> System.out.println("Response from Pong: " + response));
 
         /*Flux.interval(Duration.ofSeconds(1))
                 .flatMap(i -> Flux.merge(
